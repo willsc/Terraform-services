36cab0cf70c5176436c81cf5e2fa0f01d8b65356
C Wills <>
2019-07-07 21:21:35 +0100

    Test Terraform modules

---
+++ terraform-fastly-1.1/modules/fastly-deployment/main.tf
@@ -0,0 +1,59 @@
+resource "fastly_service_v1" "fastly-terraform" {
+  name = "${var.fastly-service-name}"
+  force_destroy = true
+  default_ttl = "${var.defaultTtl}"
+  
+
+  domain {
+    name    = "${var.domain_name}"
+    comment = "demo"
+  }
+
+  backend {
+    address = "${var.address_1}"
+    name    = "backend_1"
+    port    = 80
+  }
+
+
+backend {
+    address = "${var.address_2}"
+    name    = "backend_2"
+    port    = 80
+  }
+
+
+
+backend {
+    address = "${var.address_3}"
+    name    = "backend_3"
+    port    = 80
+  }
+
+
+backend {
+    address = "${var.address_4}"
+    name    = "backend_4"
+    port    = 80
+  }
+
+
+
+  header {
+    destination = "http.x-amz-request-id"
+    type        = "cache"
+    action      = "delete"
+    name        = "remove x-amz-request-id"
+  }
+
+  gzip {
+    name          = "file extensions and content types"
+    extensions    = ["css", "js"]
+    content_types = ["text/html", "text/css"]
+  }
+
+
+  force_destroy = true
+
+
+}
---
+++ terraform-fastly-1.2-TF-12/terraform.tfvars
@@ -0,0 +1,8 @@
+
+address_1 = "ec2-34-245-107-100.eu-west-1.compute.amazonaws.com"
+
+address_2 = "ec2-63-32-99-16.eu-west-1.compute.amazonaws.com"
+
+address_3 = "ec2-54-246-154-199.eu-west-1.compute.amazonaws.com"
+
+address_4 = "ec2-54-246-203-224.eu-west-1.compute.amazonaws.com"
